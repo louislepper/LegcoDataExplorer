2acc5320032eae7545940d0d38b8e79f3fa66d36
Louis Lepper <me@louislepper.co.nz>
2014-11-22 11:30:28 +0800

    Cleaned it up.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //Global variables.
 var xmlDoc; //At the moment this script is limited to a single xml doc.
-var mainTable; //I think I should get rid of this.
 
 var numTableMembers = 0; //Number of members displayed in the table
 
@@ -49,17 +48,18 @@ function pullXML()
 				} 
 			};
 
-	xmlhttp.onprogress=function(evt){
+	xmlhttp.onprogress=function(evt)
+	{
 		if (evt.lengthComputable) 
 		   {//evt.loaded the bytes browser receive
 			//evt.total the total bytes seted by the header
 			//
-		     var percentComplete = (evt.loaded / evt.total)*100;  
-		     $('#progressbar').progressbar("value", percentComplete );
+			 var percentComplete = (evt.loaded / evt.total)*100;  
+			 $('#progressbar').progressbar("value", percentComplete );
 		   } else {
-		   	//alert("indeterminant");
+			//alert("indeterminant");
 		   }
-	}
+	};
 
 	xmlhttp.open("GET","./combined.xml",true);
 	xmlhttp.send();			
@@ -74,97 +74,93 @@ function xmlReady()
 	getMembers();
 	populateSelectBox();
 	populateMemberDialog();
-	makeTable();
+	//makeTable();
+	rebuildTable();
 	enablePage();
 }
 
 function xmlFail()
 {
-	alert("XML not found.");
+	alert("XML not found. Please contact system administrator.");
 }
 
 function getMembers()
 {
-  var i;
+	var i;
 	xmlMemberCount = 0;
 	var x=xmlDoc.getElementsByTagName("member");
 
+	//This is simply used to avoid putting duplicates in out memberLists. It is used as a hashtable.
 	var dedupedList = {};
 
 	for(i = 0; i < x.length; i++){
-		if (typeof dedupedList[x[i].getAttribute('name-en')] == 'undefined')
-		{
-			dedupedList[x[i].getAttribute('name-en')] = 1;
+		//If we haven't already put this member's english name in our hashtable. Then we should add both their english and chinese name to our respective lists.
+		if (typeof dedupedList[x[i].getAttribute('name-en')] == 'undefined'){
+			dedupedList[x[i].getAttribute('name-en')] = 1; //Just put something that isn't 'undefined' in that slot of the HT
 			memberListEN[xmlMemberCount] = x[i].getAttribute('name-en');
 			memberListCN[xmlMemberCount] = x[i].getAttribute('name-ch');
 			xmlMemberCount++;
-
 		}
 	}
-
-	// if ( window.console && window.console.log ) {
-	// 	for(i = 0; i < xmlMemberCount; i++) {
-	// 		console.log("Member " + (i+1) + ": " + memberListEN[i] + " --- " + memberListCN[i]);
-	// 	}
-	// }
-
 }
 
 function populateSelectBox()
 {
-
+	//Make a list of all English and Chinese names.
 	var availableMembers = memberListEN.concat(memberListCN);
 
 	$( "#voterList" ).autocomplete({
-		source: availableMembers
-	});
-
-	$( "#voterList" ).autocomplete({
+		source: availableMembers,
+		/* When the user selects an autocomplete option: */
 		select: function( event, ui ) {
+			//set the value of the textbox to the autocompletion
 			$('#voterList').val(ui.item.value);
 			var v = nameToNumAndEN(ui.item.value);
+			//If the name is in the english list then it'll return the member number, otherwise -1.
 			if(v != -1) {addMember(v, false);}
 			else {
+				//The name must be Chinese. Get the member number, and add that member.
 				v = nameToNumAndCN(ui.item.value);
 				if(v != -1) {addMember(v, true);}
 				else {alert("This should never happen.");}
 			}
-			//Clear the voter selector
-			//$('#voterList').val("");
-		}
-	});
-	$( "#voterList" ).autocomplete({
+		},
 		close: function( event, ui ) {
-			//Clear the voter selector
+			//Clear the voter selector when the autocomplete closes.
 			$('#voterList').val("");
 		}
 	});
 
 }
 
-function nameToNumAndEN(name){
+/* Return the id number of a member, if their name is English. If it's not then return -1 */
+function nameToNumAndEN(name)
+{
 	var i;
 	for(i = 0; i < memberListEN.length; i++){
 		if(memberListEN[i] === name) {return i;}
 	}
-	// for(i = 0; i < memberListCN.length; i++){
-	// 	if(memberListCN[i] === name) {return i;}
-	// }
 	return -1;
 }
 
-function nameToNumAndCN(name){
+/* Return the id number of a member, if their name is Chinese. If it's not then return -1 */
+function nameToNumAndCN(name)
+{
 	var i;
-	// for(i = 0; i < memberListEN.length; i++){
-	// 	if(memberListEN[i] === name) {return i;}
-	// }
 	for(i = 0; i < memberListCN.length; i++){
 		if(memberListCN[i] === name) {return i;}
 	}
 	return -1;
 }
 
-function populateMemberDialog () 
+function nameToNum(name)
+{
+	var a = nameToNumAndEN(name);
+	if(a == -1) return nameToNumAndCN(name);
+	return a;
+}
+
+function populateMemberDialog() 
 {
 	var memberDialog = document.getElementById("memberDialog");
 	for(var i = 0; i < xmlMemberCount; i++){
@@ -174,7 +170,10 @@ function populateMemberDialog ()
 		var name = document.createElement("LABEL");
 		name.innerHTML = memberListEN[i] + " --- " + memberListCN[i];
 		checkboxO.type = "checkbox";
-		//alert(i);
+
+		/* I know that when making a function in a loop you have to be careful if depending on a value from the loop	*
+		 * because you will pass a reference, and the value may change as the loop iterates.							*
+		 * As far as I know, the function is just passed the onchange event, so I get all the needed info from that 	*/
 		checkboxO.onchange = function(e){
 			var innerCheckbox = e.target;
 			if(innerCheckbox.checked){ //The box has *just* been checked. We need to add the member.
@@ -182,9 +181,8 @@ function populateMemberDialog ()
 			} else {
 				removeMemberByMemberNum(innerCheckbox.id);
 			}
-			// alert(innerCheckbox.nextSibling);
-			//checkboxChange(innerCheckbox.id);
 		};
+
 		child.appendChild(checkboxO);
 		child.appendChild(name);
 		child.className = "memberDialogName"; 
@@ -192,15 +190,10 @@ function populateMemberDialog ()
 	}
 }
 
-function checkboxChange(memberNum){
-	addMember(memberNum);
-}
-
 function dateChange()
 {
 	updateGlobalDateObjects();
 	rebuildTable();
-	//Need to repopulate member's results.
 }
 
 function rebuildTable(){
@@ -215,17 +208,17 @@ function rebuildTable(){
 
 	var cell1 = row.insertCell(0);
 	cell1.innerHTML = "Motion/Member: (Click to remove)";
+
+	//Put all the members that were recently displayed back into the table
 	for(i = 0; i < currentlyDisplayedMembers.length; i++)
 	{
 		var memberNameCell = row.insertCell(i + 1);
 
 		memberNameCell.innerHTML = memberListEN[currentlyDisplayedMembers[i]]; //Breaks bilinguality. TODO:fix.
-		memberNameCell.title = currentlyDisplayedMembers[i];
 
 		//To remove a member
-		memberNameCell.onclick = function(e) {
-		 //This kind of works by a hack. We set the cell's title to its member number.
-		 removeMemberByMemberNum(e.target.title);
+		memberNameCell.onclick = function(e) {		 
+			removeMemberByMemberNum(nameToNum(e.target.innerHTML));
 		 }; 
 
 
@@ -235,19 +228,22 @@ function rebuildTable(){
 
 	for(i = 0; i < meetings.length; i++)
 	{
+		//The date of the specific meeting.
 		var mDate = meetings[i].getAttribute('start-date').split("/");
 		var mDateObj = new Date(mDate[2] + "/" + mDate[1] + "/" + mDate[0]);
 
 		if(!(startDateObj.getTime() <= mDateObj.getTime() && mDateObj.getTime() <= endDateObj.getTime())) continue;
 
 		var votes = meetings[i].childNodes;
+		//For every vote in the meeting.
 		for(o = 0; o < votes.length; o++)
 		{
 
-			if(votes[o].tagName != "vote") continue; //For some reason there are some text nodes here.
+			if(votes[o].tagName != "vote") continue; //For some reason there are some text nodes here. We need to ignore these.
 
 			var newMotionRow = newTable.insertRow(currentRow);
 			var motionNameCell = newMotionRow.insertCell(0);
+			//breaks bilinguality TODO:fix
 			motionNameCell.innerHTML = votes[o].getElementsByTagName("motion-en")[0].innerHTML;
 
 			var indivVotes = votes[o].getElementsByTagName("individual-votes")[0];
@@ -258,13 +254,16 @@ function rebuildTable(){
 				var vote = newMotionRow.insertCell(p+1);
 				vote.innerHTML = "Member not found.";
 
-				// Terribly needless complexity, I know. The programmer should be shot:
+				// Things get relatively computationally intensive here. Improvements could be made.
 				
 				for(q = 0; q < members.length; q++)
 				{
-					if(members[q].getAttribute('name-en') == memberListEN[i]) //Once again breaks bilinguality. TODO:fix
+					/* It seems that members tend to be in the same order, but if someone leaves, or someone's name changes
+					 * then we can't rely on that, so I do a linear search for the name. If these values are sorted (I haven't checked)
+					 * then we could optimise this */
+					if(members[q].getAttribute('name-en') == memberListEN[currentlyDisplayedMembers[p]]) //Once again breaks bilinguality. TODO:fix
 					{
-						vote.innerHTML = members[p].getElementsByTagName("vote")[0].innerHTML;
+						vote.innerHTML = members[q].getElementsByTagName("vote")[0].innerHTML;
 						break; 
 					}
 				}
@@ -273,27 +272,9 @@ function rebuildTable(){
 			}
 			currentRow++;
 		}
-
-		// var vote = mainTable.rows[currentMotion].insertCell(numTableMembers + 1);
-
-		// 		voteResultCell.innerHTML = "Member not found.";
-
-		// 		var indivVotes = votes[o].getElementsByTagName("individual-votes")[0];
-		// 		var members = indivVotes.getElementsByTagName("member");
-		// 		for(var p = 0; p < members.length; p++)
-		// 		{
-		// 			if(members[p].getAttribute('name-en') == memberName || members[p].getAttribute('name-ch') == memberName)
-		// 			{
-		// 				voteResultCell.innerHTML = members[p].getElementsByTagName("vote")[0].innerHTML;
-		// 				break; 
-		// 			}
-		// 		}
-
 	}
 	//Get current table:
 	var oldTable = document.getElementById("mainTable");
-
-	mainTable = newTable;
 
 	//Add the main newTable to the page
 	document.body.replaceChild(newTable, oldTable);
@@ -310,48 +291,6 @@ function updateGlobalDateObjects()
 	endDateObj = new Date(endDate[2] + "/" + endDate[1] + "/" + endDate[0]);
 }
 
-/*Fill the list of motions out */
-function makeTable()
-{
-	mainTable = document.createElement("TABLE");
-
-	mainTable.id = "mainTable";
-	
-	// Create an empty <tr> element and add it to the 1st position of the table:
-	var row = mainTable.insertRow(0);
-	var currentRow = 1;
-
-	var cell1 = row.insertCell(0);
-	cell1.innerHTML = "Motion/Member: (Click to remove)";
-	//Now make a new row for each motion:
-	var meetings = xmlDoc.getElementsByTagName("meeting");
-
-	for(var i = 0; i < meetings.length; i++)
-	{
-		var mDate = meetings[i].getAttribute('start-date').split("/");
-		var mDateObj = new Date(mDate[2] + "/" + mDate[1] + "/" + mDate[0]);
-
-		if(!(startDateObj.getTime() <= mDateObj.getTime() && mDateObj.getTime() <= endDateObj.getTime())) continue;
-
-		var votes = meetings[i].childNodes;
-		for(var o = 0; o < votes.length; o++){
-
-			if(votes[o].tagName != "vote") continue; //For some reason there are some text nodes here.
-
-			var newMotionRow = mainTable.insertRow(currentRow);
-			var motionNameCell = newMotionRow.insertCell(0);
-			motionNameCell.innerHTML = votes[o].getElementsByTagName("motion-en")[0].innerHTML;
-			currentRow++;
-		}
-
-	}
-	//Get current table:
-	var oldTable = document.getElementById("mainTable");
-
-	//Add the main mainTable to the page
-	document.body.replaceChild(mainTable, oldTable);
-}
-
 function enablePage(){
 	document.getElementById("memberDialogButton").disabled = "";
 	document.getElementById("startDate").disabled = "";
@@ -360,6 +299,7 @@ function enablePage(){
 
 function memberDisplayed(memberNum)
 {
+	//Jquery shortcut
 	var found = $.inArray(memberNum, currentlyDisplayedMembers);
 	if(found >= 0){
 		return true;
@@ -370,58 +310,55 @@ function removeMemberByMemberNum(memberNum)
 {
 	var i;
 	var tableIndex = -1;
-	//First find the table index.
+	//First find the table index. The table index of a member will always be their position in currentlyDisplayedMembers + 1.
 	for(i = 0; i < currentlyDisplayedMembers.length; i++)
 	{
-		// alert(currentlyDisplayedMembers[i] + "(from list of memnum) -- (from input)" + memberNum);
 		if(currentlyDisplayedMembers[i] == memberNum)
 		{
-
 			tableIndex = i+1;
-			// alert(tableIndex);
 		}
 	} 
+
 	if(tableIndex === -1) 
 	{
 			alert("Trying to remove a member that can't be found.");
 			alert(memberNum);
 			alert(memberListEN[memberNum]);
 			alert(currentlyDisplayedMembers);
-
 	}
+
 	removeMemberByTableIndex(tableIndex);
-	//Remove member from the currently displayed members list.
+	//Remove member from the currently displayed members list. Must keep the order.
 	currentlyDisplayedMembers.splice((tableIndex-1), 1);
 	deCheckMember(memberNum);
 }
+
 function removeMemberByTableIndex(index) //Only to be used by removeMemberByMemberNum, because the arrays left must be spiced. 
 {
+	var mainTable = document.getElementById("mainTable");
 	for(var i = 0; i < mainTable.rows.length; i++){
 			mainTable.rows[i].deleteCell(index);
 		 }
 	numTableMembers--;
 }
 
-// function removeMemberFromArrays(memberNu)
 
 function deCheckMember(memberNum){
+	//The id of each checkbox is the id of its associated member.
 	var checkb = document.getElementById(memberNum);
 	checkb.checked = "";
 }
 
 function addMember(memberNum, inChinese)
 {
+	var mainTable = document.getElementById("mainTable");
 
 	if(!memberDisplayed(memberNum))
 	{
-		// var voterSelector = document.getElementById("voterList");
-
 		var meetings = xmlDoc.getElementsByTagName("meeting");
 
 		var memberNameCell = mainTable.rows[0].insertCell(numTableMembers+1);
 
-
-		// memberNameCell.innerHTML = voterSelector.value;
 		var memberName;
 		if(!inChinese)
 		{
@@ -431,15 +368,12 @@ function addMember(memberNum, inChinese)
 			memberName = memberListCN[memberNum];
 		}
 		
-
 		memberNameCell.innerHTML = memberName;
-		memberNameCell.title = memberNum;
 
 		//To remove a member
 		memberNameCell.onclick = function(e) {
-		 //This once again works kind of by hack. We just store the member number in the cell's title.
-		 removeMemberByMemberNum(e.target.title);
-		 }; 
+			removeMemberByMemberNum(nameToNum(e.target.innerHTML));
+		}; 
 
 		var currentMotion = 1;
 
@@ -448,6 +382,7 @@ function addMember(memberNum, inChinese)
 			var mDate = meetings[i].getAttribute('start-date').split("/");
 			var mDateObj = new Date(mDate[2] + "/" + mDate[1] + "/" + mDate[0]);
 
+			//There's room for optimisation here. Currently going through *all* motions, and just skipping ones that aren't in the date range.
 			if(!(startDateObj.getTime() <= mDateObj.getTime() && mDateObj.getTime() <= endDateObj.getTime())) continue;
 
 			var votes = meetings[i].childNodes;
@@ -479,9 +414,11 @@ function addMember(memberNum, inChinese)
 			}
 
 		}
+
 		var checkb = document.getElementById(memberNum);
 		checkb.checked = "checked";
 		currentlyDisplayedMembers.push(memberNum);
 		numTableMembers++;
+		
 	} else alert("Member already displayed.");
 }
